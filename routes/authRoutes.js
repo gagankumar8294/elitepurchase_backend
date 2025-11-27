f76e65f343674c73c75d08386c88d13908b32cfc
gagankumar8294 <gagankumar8294@gmail.com>
2025-11-27 20:58:07 +0530

    redirecting to homepage after loggedout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -37,7 +37,7 @@ Authrouter.get("/logout", (req, res) => {
   req.logout((err) => {
     if (err) return res.status(500).json({ message: "Logout error" });
     res.clearCookie("connect.sid");
-    res.json({ message: "Logged out successfully" });
+    res.redirect("https://elitepurchase.in");
   });
 });
 
